24fd3840904f938f730aad480bb85efb7336cd67
Waqasahmad9300 <108908697+Waqasahmad9300@users.noreply.github.com>
2022-07-22 17:09:32 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.owner = ['923077889300']
+global.premium = ['923077889300']
+global.ownernomer = '923077889300'
+global.ownername = 'Kashii9300'
+global.botname = 'Kashii Lover'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
 global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
-global.packname = 'Queen Bixby MD'
-global.author = 'DARK ALPHA'
-global.sessionName = 'session'
+global.packname = 'Kashii 💕 Mishii'
+global.author = 'AKASHII MISHII'
+global.sessionName = 'session1'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
 global.mess = {
